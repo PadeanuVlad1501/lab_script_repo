0fdf1918f14bc1dcb38df51a9a837c6e9ad7055b
PadeanuVlad1501 <padeanuvladflorin@gmail.com>
2026-04-15 18:26:31 +0300

    Am schimbat denumirea folderului creeat de script in ScoutSuite ca sa nu aiba numele identic cu tool-ul

--- ScoutSuite_lab/scoutsuite_vm_config.sh
+++ ScoutSuite_lab/scoutsuite_vm_config.sh
@@ -10,23 +10,23 @@ echo "[*] Installing AWS CLI v2..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
 sudo ./aws/install
-rm -rf awscliv2.zip aws/ 
+rm -rf awscliv2.zip aws/
 
 echo "[*] Downloading aws_cloudformation_configuration.yaml from GitHub..."
 curl -sS "https://raw.githubusercontent.com/PadeanuVlad1501/lab_script_repo/main/ScoutSuite_lab/cloudformation_config.yaml" -o "/home/ubuntu/BnB/scoutsuite/cloudformation_config.yaml"
 
 echo "[*] Creating lab directories in /home/ubuntu/BnB..."
-mkdir -p /home/ubuntu/BnB/scoutsuite
+mkdir -p /home/ubuntu/BnB/ScoutSuite
 
 echo "[*] Downloading aws_cloudformation_configuration.yaml from GitHub..."
 curl -sS "https://raw.githubusercontent.com/PadeanuVlad1501/lab_script_repo/main/ScoutSuite_lab/cloudformation_config.yaml" -o "/home/ubuntu/BnB/scoutsuite/cloudformation_config.yaml"
 
 echo "[*] Creating the Python Virtual Environment..."
-cd /home/ubuntu/BnB/scoutsuite
+cd /home/ubuntu/BnB/ScoutSuite
 python3 -m venv venv
 
 echo "[*] Fixing permissions for the ubuntu user..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/BnB/scoutsuite
+sudo chown -R ubuntu:ubuntu /home/ubuntu/BnB/ScoutSuite
 
 echo "[+] Setup Complete! AWS CLI and the venv are ready."
 aws --version
