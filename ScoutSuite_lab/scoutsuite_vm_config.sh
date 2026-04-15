0fbdf6d3a0c6b81fd2d1a8b4fac9412a9fcf96a2
PadeanuVlad1501 <padeanuvladflorin@gmail.com>
2026-04-15 18:50:50 +0300

    changed bug in scoutsuite script

--- ScoutSuite_lab/scoutsuite_vm_config.sh
+++ ScoutSuite_lab/scoutsuite_vm_config.sh
@@ -13,13 +13,13 @@ sudo ./aws/install
 rm -rf awscliv2.zip aws/
 
 echo "[*] Downloading aws_cloudformation_configuration.yaml from GitHub..."
-curl -sS "https://raw.githubusercontent.com/PadeanuVlad1501/lab_script_repo/main/ScoutSuite_lab/cloudformation_config.yaml" -o "/home/ubuntu/BnB/scoutsuite/cloudformation_config.yaml"
+curl -sS "https://raw.githubusercontent.com/PadeanuVlad1501/lab_script_repo/main/ScoutSuite_lab/cloudformation_config.yaml" -o "/home/ubuntu/BnB/ScoutSuite/cloudformation_config.yaml"
 
 echo "[*] Creating lab directories in /home/ubuntu/BnB..."
 mkdir -p /home/ubuntu/BnB/ScoutSuite
 
 echo "[*] Downloading aws_cloudformation_configuration.yaml from GitHub..."
-curl -sS "https://raw.githubusercontent.com/PadeanuVlad1501/lab_script_repo/main/ScoutSuite_lab/cloudformation_config.yaml" -o "/home/ubuntu/BnB/scoutsuite/cloudformation_config.yaml"
+curl -sS "https://raw.githubusercontent.com/PadeanuVlad1501/lab_script_repo/main/ScoutSuite_lab/cloudformation_config.yaml" -o "/home/ubuntu/BnB/ScoutSuite/cloudformation_config.yaml"
 
 echo "[*] Creating the Python Virtual Environment..."
 cd /home/ubuntu/BnB/ScoutSuite
