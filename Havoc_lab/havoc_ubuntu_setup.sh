6cb7f4b301073760ceeca903d723e20e66d2ad03
PadeanuVlad1501 <padeanuvladflorin@gmail.com>
2026-04-23 22:09:59 +0300

    updated havoc ubuntu setup

--- Havoc_lab/havoc_ubuntu_setup.sh
+++ Havoc_lab/havoc_ubuntu_setup.sh
@@ -1,47 +1,36 @@
 #!/bin/bash
 
-echo "[*] Starting Havoc C2 environment setup..."
+echo "[*] Starting Setup"
 
-# 1. Update
-echo "[*] Updating apt..."
-apt-get update -y
+#  qt5 dependencies
+echo "[*] Installing  Qt5 dependencies..."
+sudo apt-get update -y
+sudo apt-get install -y qtbase5-dev libqt5websockets5-dev qtdeclarative5-dev
 
-echo "[*] Installing minimal dependencies..."
-sudo apt-get install -y git golang cmake make gcc g++ nasm python3 python3-dev python3-websockets \
-    qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5-dev qtdeclarative5-dev libqt5svg5-dev \
-    libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev \
-    libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libxcb-glx0-dev libxcb-keysyms1-dev \
-    libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync-dev libxcb-xfixes0-dev \
-    libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-util-dev \
-    libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
+# creating lab folder
+echo "[*] Preparing system directories..."
+sudo mkdir -p /opt/havoc-client
+cd /opt/havoc-client
 
+# havoc binary archive
+echo "[*] Downloading pre-compiled Havoc-Client framework"
+sudo wget -O havoc-client.tar.gz "https://github.com/PadeanuVlad1501/lab_script_repo/raw/refs/heads/main/Havoc_lab/havoc-client.tar.gz."
 
+#  decompressing tar archive
+echo "[*] Extracting resources."
+sudo tar -xzvf havoc-client.tar.gz
 
-echo "[*] Cleaning up apt cache to free disk space..."
-sudo apt-get clean
+# adding executable permission
+echo "[*] Configurating permissions..."
+sudo chmod +x Havoc
 
-echo "[*] Creating directory structure in /home/ubuntu/BnB..."
-mkdir -p /home/ubuntu/BnB
-cd /home/ubuntu/BnB
+# creating symlink
+echo "[*] Creating global havoc command"
+sudo ln -sf /opt/havoc-client/Havoc /usr/local/bin/havoc-client
 
+# cleanup
+sudo rm havoc-client.tar.gz
 
-if [ ! -d "Havoc" ]; then
-    echo "[*] Cloning Havoc..."
-    git clone --depth 1 https://github.com/HavocFramework/Havoc.git
-else
-    echo "[!] Havoc directory already exists. Skipping clone."
-fi
-
-echo "[*] Compiling components..."
-cd Havoc
-
-echo "[*] Building Teamserver..."
-make ts-build
-
-echo "[*] Building Client UI..."
-make client-build
-
-echo "[*] Cleaning Go build cache..."
-go clean -cache -modcache
-
-echo "[+] Done! Havoc is installed in /home/ubuntu/BnB/Havoc"
+echo "================================================================"
+echo "[+] Setup completed!"
+echo "================================================================"
