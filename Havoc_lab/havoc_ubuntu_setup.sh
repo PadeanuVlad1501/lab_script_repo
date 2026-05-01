fad26876d8cc4c753048042bfd04c1aedc9acc2c
PadeanuVlad1501 <padeanuvladflorin@gmail.com>
2026-05-01 15:22:06 +0300

    revamped ubuntu setup file

--- Havoc_lab/havoc_ubuntu_setup.sh
+++ Havoc_lab/havoc_ubuntu_setup.sh
@@ -2,33 +2,29 @@
 
 echo "[*] Starting Setup"
 
-#  qt5 dependencies
-echo "[*] Installing  Qt5 dependencies..."
+echo "[*] Installing Qt5 dependencies..."
 sudo apt-get update -y
 sudo apt-get install -y qtbase5-dev libqt5websockets5-dev qtdeclarative5-dev
 
-# creating lab folder
 echo "[*] Preparing system directories..."
-sudo mkdir -p /opt/havoc-client
-cd /opt/havoc-client
+sudo mkdir -p /home/ubuntu/BnB/Havoc
+cd /home/ubuntu/BnB/Havoc
 
-# havoc binary archive
 echo "[*] Downloading pre-compiled Havoc-Client framework"
 sudo wget -O havoc-client.tar.gz "https://github.com/PadeanuVlad1501/lab_script_repo/raw/refs/heads/main/Havoc_lab/havoc-client.tar.gz"
 
-#  decompressing tar archive
 echo "[*] Extracting resources."
 sudo tar -xzvf havoc-client.tar.gz
 
-# adding executable permission
 echo "[*] Configurating permissions..."
 sudo chmod +x Havoc
 
-# creating symlink
-echo "[*] Creating global havoc command"
-sudo ln -sf /opt/havoc-client/Havoc /usr/local/bin/havoc-client
+echo "[*] Fixing directory permissions..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/BnB/Havoc
 
-# cleanup
+echo "[*] Creating global havoc command"
+sudo ln -sf /home/ubuntu/BnB/Havoc/Havoc /usr/local/bin/havoc-client
+
 sudo rm havoc-client.tar.gz
 
 echo "================================================================"
