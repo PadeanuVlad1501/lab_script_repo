9292bfb67d6ac7117e13bf6efc4ad10483bb1a68
Padeanu Vlad <128638106+PadeanuVlad1501@users.noreply.github.com>
2026-06-04 22:34:55 +0300

    minor changes

--- Leviathan_lab/leviathan_windows_setup.ps1
+++ Leviathan_lab/leviathan_windows_setup.ps1
@@ -12,12 +12,11 @@ if (!(Test-Path -Path $LabDir)) {
     New-Item -ItemType Directory -Path $LabDir -Force | Out-Null
 }
 
-# Download the Activation Script from your personal repository
-# !!! REPLACE THE URL BELOW WITH YOUR ACTUAL RAW GITHUB URL !!!
-$ScriptUrl = "YOUR_RAW_GITHUB_URL_HERE"
-$OutPath = "$LabDir\Start_Leviathan_Lab.ps1"
+# Download the Activation Script
+$ScriptUrl = "https://raw.githubusercontent.com/PadeanuVlad1501/lab_script_repo/refs/heads/main/Leviathan_lab/leviathan_lab_start.ps1"
+$OutPath = "$LabDir\leviathan_windows_setup.ps1"
 
 Write-Host "[*] Downloading activation script from repository..."
 Invoke-WebRequest -Uri $ScriptUrl -OutFile $OutPath
 
-Write-Host "[+] Base setup complete. The VM can now be snapshotted."
+Write-Host "[+] Lab setup complete !"
